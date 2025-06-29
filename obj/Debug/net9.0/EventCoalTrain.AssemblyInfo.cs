e23c6a54f61198b2f4215c5ae9b1f098ca36543f
WhiteTowerGames <whitetowersoftware@gmail.com>
2025-06-29 19:47:02 +0300

    Update assembly informational version and adjust project file paths

--- obj/Debug/net9.0/EventCoalTrain.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventCoalTrain.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025 WhiteTowerGames")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Event system for Godot (.NET 9.0) and Unity (.NET Standard 2.1).")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f18e1c1b977bb9d73d2c8fbcbf7c02d4daa8673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c97ae5b02fe7309783b26053a13844c4add0b60")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventCoalTrain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventCoalTrain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
